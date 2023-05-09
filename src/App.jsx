1ad108e610515cdd330b2d9fbe5002d3d2963883
Carlos Oliveira <carlos.burndev@gmail.com>
2023-05-08 23:39:42 -0300

    feat(Post): adds author and content props

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,23 @@ function App() {
   return (   
     <>
       <h1>Hello World</h1>  
-       <Post/>
-       <Post/>
-       <Post/>
+      <Post 
+        author="Carlos Oliveira"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, quae saepe libero labore alias dolorum ad sapiente. Perspiciatis non et, suscipit animi officia asperiores itaque eligendi culpa consectetur ullam enim."
+      />
+
+      <Post
+        author="Fulano Oliveira"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, quae saepe libero labore alias dolorum ad sapiente. Perspiciatis non et, suscipit animi officia asperiores itaque eligendi culpa consectetur ullam enim."
+      />
+      
+      <Post
+        author="Ciclano Oliveira"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, quae saepe libero labore alias dolorum ad sapiente. Perspiciatis non et, suscipit animi officia asperiores itaque eligendi culpa consectetur ullam enim."
+      />
     </>
   )
 }
 
 export default App
+
